d93afc91a14302cea048393696aef5d5da355c28
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-03-26 08:54:09 +0100

    Updated: reload.bash

--- reload.bash
+++ reload.bash
@@ -21,6 +21,7 @@ mysql -u root < "${BASEDIR}/00-extra/db/create-empty-database.sql"
 rm -rf /app/symfony2app/app/cache/*
 
 php "${BASEDIR}/app/console" doctrine:schema:update --force
+php "${BASEDIR}/app/console" doctrine:fixtures:load
 
 sudo chmod -R 0777 /app/symfony2app
 sudo chown -R "${WHOAMI}:${WHOAMI}" /app/symfony2app
