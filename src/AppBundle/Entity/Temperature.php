ce7cae9b8299f0f460d424c81cb320492e341a79
Pawel Czuchaj <pawlo1501@gmail.com>
2015-04-08 21:55:06 +0200

    Temperature: klasa entity

---
+++ src/AppBundle/Entity/Temperature.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Temperature
+ */
+class Temperature
+{
+     /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+     /**
+     * @var float
+     *
+     * @ORM\Column(name="temperature", type="float")
+     */
+    private $temperature;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return Temperature
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set temperature
+     *
+     * @param float $temperature
+     * @return Temperature
+     */
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    /**
+     * Get temperature
+     *
+     * @return float 
+     */
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+}
