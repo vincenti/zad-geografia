e375ff7aea9fa4f9e08cfeaf78a899e109f045ab
Malgorzata Golab <golabm236@gmail.com>
2015-04-09 20:22:09 +0200

    Moon: klasa entity

---
+++ src/AppBundle/Entity/Moon.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Moon
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Moon
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="radius", type="integer")
+     */
+    private $radius;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Moon
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set radius
+     *
+     * @param integer $radius
+     * @return Moon
+     */
+    public function setRadius($radius)
+    {
+        $this->radius = $radius;
+
+        return $this;
+    }
+
+    /**
+     * Get radius
+     *
+     * @return integer 
+     */
+    public function getRadius()
+    {
+        return $this->radius;
+    }
+}
