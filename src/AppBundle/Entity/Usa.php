3e6e6a79a2f1c84488eb5a9a1005fe5ab32df742
MateuszCharkot <mateusz.charkot@gmail.com>
2015-05-09 16:22:59 +0200

    USA: klasa entity

---
+++ src/AppBundle/Entity/Usa.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usa
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Usa
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nazwa", type="string", length=255)
+     */
+    private $nazwa;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="population", type="integer")
+     */
+    private $population;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nazwa
+     *
+     * @param string $nazwa
+     * @return Usa
+     */
+    public function setNazwa($nazwa)
+    {
+        $this->nazwa = $nazwa;
+
+        return $this;
+    }
+
+    /**
+     * Get nazwa
+     *
+     * @return string 
+     */
+    public function getNazwa()
+    {
+        return $this->nazwa;
+    }
+
+    /**
+     * Set population
+     *
+     * @param integer $population
+     * @return Usa
+     */
+    public function setPopulation($population)
+    {
+        $this->population = $population;
+
+        return $this;
+    }
+
+    /**
+     * Get population
+     *
+     * @return integer 
+     */
+    public function getPopulation()
+    {
+        return $this->population;
+    }
+}
