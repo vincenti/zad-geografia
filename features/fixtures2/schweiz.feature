2d1676f8bf6cb8bf2a7ffb9fd02ed5893473cc16
vincenti <r.rzoncabarbara@gmail.com>
2015-05-30 13:19:10 +0200

    Szwajcaria

---
+++ features/fixtures2/schweiz.feature
@@ -0,0 +1,11 @@
+Feature: Fikstury dla tabeli schweiz
+
+  Scenario: Lista fixtur z tabeli schweiz
+    Given I am on "schweiz.html"
+     Then I should see "Niemiecki"
+      And I should see "70"
+     Then I should see "Francuski"
+      And I should see "25"
+     Then I should see "Angielski"
+      And I should see "5"
+
---
+++ src/AppBundle/Controller/MySchweizController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MySchweizController extends Controller
+{
+
+    /**
+     * @Route("/schweiz.html", name="schweiz")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Schweiz')->findAll();
+
+        return $this->render(
+            'default/schweiz.html.twig',
+            array(
+                'entities' => $entities,
+            )
+        );
+    }
+
+} 
